20b314d6febf8adc1f29e8366d9e56075442bf73
YashOO7-cyb <yash.chandankhede207@gmail.com>
2026-03-26 17:46:42 +0530

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    target: 'esnext',
+    minify: 'esbuild'
+  },
+  server: {
+    port: 5173
+  }
+});
+
+
+
